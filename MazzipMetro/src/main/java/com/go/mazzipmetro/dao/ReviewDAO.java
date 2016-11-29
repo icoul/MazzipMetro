5ae212edae85ebc98d78dacabad9421df232d601
nos0915 <n89578425@gmail.com>
2016-11-29 16:50:54 +0900

    음식점 상세페이지 추가

--- MazzipMetro/src/main/java/com/go/mazzipmetro/dao/ReviewDAO.java
+++ MazzipMetro/src/main/java/com/go/mazzipmetro/dao/ReviewDAO.java
@@ -1,5 +1,8 @@
 package com.go.mazzipmetro.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,5 +11,40 @@ import org.springframework.stereotype.Repository;
 public class ReviewDAO implements IDAO{
 	
 	@Autowired
-	private SqlSessionTemplate sqlSession;
+	private SqlSessionTemplate sqlsession;
+	
+	public HashMap<String,String> getRestaurant(String restseq) {
+		HashMap<String,String> restvo = sqlsession.selectOne("review.getRestaurant", restseq);
+		return restvo;
+	}
+
+	public List<HashMap<String,String>> getReviewList(String restseq) {
+		List<HashMap<String,String>> reviewList = sqlsession.selectList("review.getReviewList", restseq);
+		return reviewList;
+	}
+
+	public List<HashMap<String,String>> getReviewImageList() {
+		List<HashMap<String,String>> reviewImageList = sqlsession.selectList("review.getReviewImageList");
+		return reviewImageList;
+	}
+
+	public List<HashMap<String, String>> getReviewImageList(String reviewseq) {
+		List<HashMap<String,String>> reviewImageList = sqlsession.selectList("review.getReviewImageListByReviewSeq",reviewseq);
+		return reviewImageList;
+	}
+
+	public String getLargeReviewImageName(String revimgseq) {
+		String reviewImageName = sqlsession.selectOne("review.getLargeReviewImageName",revimgseq);
+		return reviewImageName;
+	}
+
+	public List<HashMap<String, String>> getAgeLineChartList(String restseq) {
+		List<HashMap<String,String>> agelineChartList = sqlsession.selectList("review.getAgeLineChartList", restseq);
+		return agelineChartList;
+	}
+
+	public List<HashMap<String, String>> getGenderChartList(String restseq) {
+		List<HashMap<String,String>> genderChartList = sqlsession.selectList("review.getGenderChartList", restseq);
+		return genderChartList;
+	}
 }
--- MazzipMetro/src/main/java/com/go/mazzipmetro/service/ReviewService.java
+++ MazzipMetro/src/main/java/com/go/mazzipmetro/service/ReviewService.java
@@ -1,14 +1,52 @@
 package com.go.mazzipmetro.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.go.mazzipmetro.dao.ReviewDAO;
+
 
 @Service
 public class ReviewService implements IService{
 
 	@Autowired
 	private ReviewDAO dao;
-	
+
+	public HashMap<String,String> getRestaurant(String restseq) {
+		HashMap<String,String> restvo = dao.getRestaurant(restseq);
+		return restvo;
+	}
+
+	public List<HashMap<String,String>> getReviewList(String restseq) {
+		List<HashMap<String,String>> reviewList = dao.getReviewList(restseq);
+		return reviewList;
+	}
+
+	public List<HashMap<String,String>> getReviewImageList() {
+		List<HashMap<String,String>> reviewImageList = dao.getReviewImageList();
+		return reviewImageList;
+	}
+
+	public List<HashMap<String, String>> getReviewImageList(String reviewseq) {
+		List<HashMap<String,String>> reviewImageList = dao.getReviewImageList(reviewseq);
+		return reviewImageList;
+	}
+
+	public String getLargeReviewImageName(String revimgseq) {
+		String reviewImageName = dao.getLargeReviewImageName(revimgseq);
+		return reviewImageName;
+	}
+
+	public List<HashMap<String, String>> getAgeLineChartList(String restseq) {
+		List<HashMap<String,String>> agelineChartList = dao.getAgeLineChartList(restseq);
+		return agelineChartList;
+	}
+
+	public List<HashMap<String, String>> getGenderChartList(String restseq) {
+		List<HashMap<String,String>> genderChartList = dao.getGenderChartList(restseq);
+		return genderChartList;
+	}
 }
